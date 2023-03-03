6ecb0b61d2fdd39a6497c0a757235ab501ea6fdc
Fernando Gomez <redchameleonapps@gmail.com>
2023-03-02 23:09:12 -0700

    Created a color cycling cycle to see how SwiftUI renders 100 screens

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,50 +7,89 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
 
-    // How wide to make each petal
-    var petalWidth: Double = 100
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
 
-    func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
-        var path = Path()
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
 
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
-
-            // move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-
-            // add it to our main path
-            path.addPath(rotatedPetal)
-            
+        if targetHue > 1 {
+            targetHue -= 1
         }
 
-        // now send the main path back
-        return path
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
+//struct Flower: Shape {
+//    // How much to move this petal away from the center
+//    var petalOffset: Double = -20
+//
+//    // How wide to make each petal
+//    var petalWidth: Double = 100
+//
+//    func path(in rect: CGRect) -> Path {
+//        // The path that will hold all petals
+//        var path = Path()
+//
+//        // Count from 0 up to pi * 2, moving up pi / 8 each time
+//        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+//            // rotate the petal by the current value of our loop
+//            let rotation = CGAffineTransform(rotationAngle: number)
+//
+//            // move the petal to be at the center of our view
+//            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+//
+//            // create a path for this petal using our properties plus a fixed Y and height
+//            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+//
+//            // apply our rotation/position transformation to the petal
+//            let rotatedPetal = originalPetal.applying(position)
+//
+//            // add it to our main path
+//            path.addPath(rotatedPetal)
+//
+//        }
+//
+//        // now send the main path back
+//        return path
+//    }
+//}
+
 struct ContentView: View {
-    
-  
-        var body: some View {
-            Capsule()
-                .strokeBorder(ImagePaint(image: Image("download"), scale: 0.1), lineWidth: 20)
-                .frame(width: 300, height: 200)
-        }
-    }
+    @State private var colorCycle = 0.0
+
+       var body: some View {
+           VStack {
+               ColorCyclingCircle(amount: colorCycle)
+                   .frame(width: 300, height: 300)
+
+               Slider(value: $colorCycle)
+           }
+       }
+   }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
