20ccd76ff503bde8ad5b339b92c9b7641dc42d71
Fernando Gomez <redchameleonapps@gmail.com>
2023-03-06 23:42:19 -0700

    animated complex shapes

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -87,7 +87,7 @@ struct Trapezoid: Shape {
     }
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        
+
         path.move(to: CGPoint(x: 0, y: rect.maxY))
                path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
                path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
@@ -95,6 +95,35 @@ struct Trapezoid: Shape {
                path.addLine(to: CGPoint(x: 0, y: rect.maxY))
 
                return path
+    }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
     }
 }
 
